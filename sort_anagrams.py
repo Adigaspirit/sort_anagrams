2110d24e175b6b7090ac1c668489bbf46489516d
Adigaspirit <Adigaspirit@gmail.com>
2019-02-10 09:41:43 +0000

    Create sort_anagrams.py

---
+++ sort_anagrams.py
@@ -0,0 +1,14 @@
+def sort_anagrams(list_of_strings):
+    container = []
+    j=0
+    while j < len(list_of_strings):
+        i = 1
+        rev_list = [list_of_strings[j]]
+        while i < len(list_of_strings):
+            if sorted(list(list_of_strings[j])) == sorted(list(list_of_strings[i])):
+                rev_list.append(list_of_strings[i])
+            i += 1
+        container.append(rev_list)
+        for e in rev_list:
+            list_of_strings.remove(e)
+    return container
