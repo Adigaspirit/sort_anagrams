d0c3d98383fcfc59824cb775245ea0391e092686
Adigaspirit <Adigaspirit@gmail.com>
2019-02-10 20:37:30 +0000

    Update sort_anagrams.py

--- sort_anagrams.py
+++ sort_anagrams.py
@@ -5,7 +5,7 @@ def sort_anagrams(list_of_strings):
         i = 1
         rev_list = [list_of_strings[j]]
         while i < len(list_of_strings):
-            if sorted(list(list_of_strings[j])) == sorted(list(list_of_strings[i])):
+            if sorted(list_of_strings[j]) == sorted(list_of_strings[i]):
                 rev_list.append(list_of_strings[i])
             i += 1
         container.append(rev_list)
